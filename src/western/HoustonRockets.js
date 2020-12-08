9bfa0e44129e40763292ded501edb84f299f795b
Rome Bell <romebell5@gmail.com>
2020-12-08 14:46:52 -0800

    [houstonrockets] Update position

--- src/western/HoustonRockets.js
+++ src/western/HoustonRockets.js
@@ -5,12 +5,12 @@ function HoustonRockets() {
     const james = {
         team: 'Houston Rockets',
         number: 13,
-        postiion: 'Point Guard'
+        position: 'Point Guard'
     }
     return (
         <>
             <h2>Houston Rockets</h2>
-            <JamesHarden position={james.postiion} number={james.number} team={james.team} />
+            <JamesHarden position={james.position} number={james.number} team={james.team} />
         </>
     );
 }
